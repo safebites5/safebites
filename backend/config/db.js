438d612747ea09aad1de6667075d5f9135469f3c
safebites <safebites5@gmail.com>
2026-05-27 17:40:34 +0700

    Add files via upload

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "safebites_preorder"
+});
+
+db.connect((err) => {
+
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Database Connected");
+    }
+
+});
+
+module.exports = db;
---
+++ backend/config/midtrans.js
@@ -0,0 +1,17 @@
+const midtransClient =
+require("midtrans-client");
+
+let snap =
+new midtransClient.Snap({
+
+    isProduction:false,
+
+    serverKey:
+    process.env.MIDTRANS_SERVER_KEY,
+
+    clientKey:
+    process.env.MIDTRANS_CLIENT_KEY
+
+});
+
+module.exports = snap;
